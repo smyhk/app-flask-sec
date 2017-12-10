f279c57391b74751b1ef93aa697e502456d9134d
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-12-10 00:04:02 -0800

    Configure database and appropriate imports

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# docker run -p 5432:5432 --name flask-postgres -e POSTGRES_PASSWORD=secretpassword -d postgres
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:secretpassword@localhost:5432/appflasksec'
+db = SQLAlchemy(app)
 
 
 @app.route("/")

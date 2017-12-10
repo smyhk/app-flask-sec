3465704eeaa3d14114818313c3563da41f45e0ad
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-12-10 13:19:40 -0800

    Add Bootstrap and assets

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
 
@@ -17,8 +17,8 @@ db = SQLAlchemy(app)
 
 # Define models
 roles_users = db.Table('roles_users',
-        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+                       db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+                       db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
 
 
 class Role(db.Model, RoleMixin):
@@ -44,22 +44,14 @@ security = Security(app, user_datastore)
 
 @app.route("/")
 def index():
-    return render_template('add_user.html')
+    return render_template('index.html')
 
 
 @app.route("/profile/<email>")
+@login_required
 def profile(email):
     user = User.query.filter_by(email=email).one()
     return render_template('profile.html', user=user)
-
-
-@app.route("/post_user", methods=['POST'])
-def post_user():
-    user = User(request.form['username'], request.form['email'])
-    db.session.add(user)
-    db.session.commit()
-
-    return redirect(url_for('index'))
 
 
 if __name__ == "__main__":

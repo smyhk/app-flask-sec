5db1e3e830d7abc53f40129bcee24cb316edb750
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-12-10 12:26:49 -0800

    Implement basic flask-security

--- app.py
+++ app.py
@@ -1,38 +1,55 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
 
 app = Flask(__name__)
 
 # docker run -p 5432:5432 --name flask-postgres -e POSTGRES_PASSWORD=secretpassword -d postgres
 # this will be loaded from a config file or env for production
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:secretpassword@localhost:5432/appflasksec'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # surpress deprecation warning
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # supress deprecation warning
+app.config['SECURITY_REGISTERABLE'] = True
+app.config['SECRET_KEY'] = 'super-secret'
+app.config['SECURITY_PASSWORD_SALT'] = '$2a$16$PnnIgfMwkOjGX4SkHqSOPO'
+
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
+# Define models
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+# Setup Flask-Security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
 
 
 @app.route("/")
 def index():
-    myUsers = User.query.all()
-    oneItem = User.query.filter_by(username="smyhk").one()
-    return render_template('add_user.html', myUsers=myUsers, oneItem=oneItem)
+    return render_template('add_user.html')
 
 
-@app.route("/profile/<username>")
-def profile(username):
-    user = User.query.filter_by(username=username).one()
+@app.route("/profile/<email>")
+def profile(email):
+    user = User.query.filter_by(email=email).one()
     return render_template('profile.html', user=user)
 
 

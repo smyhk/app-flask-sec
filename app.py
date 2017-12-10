08a5b61a11a171f41156d6b1b03a0b9396b02742
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-12-10 00:21:25 -0800

    Define User model

--- app.py
+++ app.py
@@ -3,8 +3,22 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # docker run -p 5432:5432 --name flask-postgres -e POSTGRES_PASSWORD=secretpassword -d postgres
+# this will be loaded from a config file or env for production
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:secretpassword@localhost:5432/appflasksec'
 db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r' % self.username
 
 
 @app.route("/")
